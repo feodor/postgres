5101977a2cc01a6d772a10f8b70ecea36783f7f0
Teodor Sigaev <teodor@sigaev.ru>
2012-11-09 22:24:13 +0400

    improve test for nested hstore

--- contrib/hstore/sql/nested.sql
+++ contrib/hstore/sql/nested.sql
@@ -6,3 +6,4 @@ select 'ff => {a=>12, b=>16}, qq=> 123'::hstore;
 
 select 'aa => {a,aaa}, qq=>{ a=>12, b=>16 , c=> { c1, c2}, d=>{d1=>d1, d2=>d2, d1=>d3} }'::hstore;
 
+select '"aa"=>{a,aaa}, "qq"=>{"a"=>"12", "b"=>"16", "c"=>{c1,c2}, "d"=>{"d1"=>"d1", "d2"=>"d2"}}'::hstore;
